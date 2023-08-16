7f54dfb378c043f41682779fd6f3de651225cc6e
Dario Urdaneta <dariourdaneta@gmail.com>
2023-08-16 17:12:41 -0400

    adding alt to img and removing comments of Js file

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,9 +1,5 @@
 
 // Ejercicio N1.
-// Se debe construir una página con una única imagen, al hacerle click a la imagen se
-// le debe agregar un borde rojo de 2 pixeles, al hacerle click nuevamente se le debe
-// quitar el borde.
-
 
 function clickMe() {
   const userClick = document.getElementById('image');
@@ -12,13 +8,6 @@ function clickMe() {
 
 
 // Ejercicio N2.
-//Se debe construir una página web que permita pedir hasta 10 stickers de los 3 tipos
-//disponibles. Para eso deberá existir un input debajo de cada imagen donde el
-//usuario podrá ingresar un número. En caso de que la suma de todos los stickers
-//seleccionados sea menor o igual 10 se indicará en un párrafo nuevo la cantidad de
-//stickers llevados. Ejemplo "llevas 5 stickers" en cualquier otro caso se mostrará un
-//texto que dice "Llevas demasiados stickers".
-
 
 function stickerCalculator() {
   let userSticker1 = Number(document.getElementById('sticker-1').value);
@@ -35,14 +24,7 @@ function stickerCalculator() {
   }
 }
 
-
 // Ejercicio N3.
-// Se debe construir una página con 3 select y opciones con dígitos del 1 al 9, al hacer
-// click a la opción ingresar se debe verificar que el password sea 911 y en ese caso
-// mostrar debajo del botón ingresar, un párrafo con el contenido "password 1
-// correcto", en caso de que el password sea 714 se debe mostrar en el párrafo
-// "password 2 es correcto", si ingresa otro valor se debe mostrar "password
-// incorrecto"
 
 function passwordValidation() {
   const userOption1 = Number(document.getElementById('box-1').value);
